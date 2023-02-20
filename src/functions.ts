9435298a52c4947d0f93e8691c4c64714aa0549d
Ian <ianjamessteele@gmail.com>
2023-02-20 13:54:01 -0500

    Finished and tested all functions

--- src/functions.ts
+++ src/functions.ts
@@ -12,6 +12,9 @@ export function fahrenheitToCelius(temperature: number): number {
  * if the number is greater than zero.
  */
 export function add3(first: number, second: number, third: number): number {
+    if (first < 0) first = 0;
+    if (second < 0) second = 0;
+    if (third < 0) third = 0;
     return first + second + third;
 }
 
@@ -20,7 +23,7 @@ export function add3(first: number, second: number, third: number): number {
  * mark added to the end.
  */
 export function shout(message: string): string {
-    return message.toUpperCase();
+    return message.toUpperCase() + "!";
 }
 
 /**
@@ -28,7 +31,7 @@ export function shout(message: string): string {
  * mark. Do not use an `if` statement in solving this question.
  */
 export function isQuestion(message: string): boolean {
-    return true;
+    return message[message.length - 1] === "?";
 }
 
 /**
@@ -37,5 +40,7 @@ export function isQuestion(message: string): boolean {
  * upper or lower case), then return `false`. Otherwise, return `null`.
  */
 export function convertYesNo(word: string): boolean | null {
-    return true;
+    if (word.toUpperCase() === "YES") return true;
+    else if (word.toUpperCase() === "NO") return false;
+    return null;
 }
